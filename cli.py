a6eb978965f2e916eac9d9e3e491c5871e028c9b
Andrea Viviani <andrea.viviani89@gmail.com>
2022-06-23 17:00:41 +0200

    Update cli.py

--- cli.py
+++ cli.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 # print(sys.argv)
 
-df = pd.read_csv(sys.argv)
-
+df = pd.read_csv(sys.argv[1])
 print(df.head())
+
 
 
 # df1 = pd.read_csv('jigsaw-toxic-comment-classification-challenge/train.csv/train.csv')
